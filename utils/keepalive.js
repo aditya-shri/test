89892fd3b3ce24c20aa019ee875c67569482d47e
Aditya Shrivastava <18643014+aditya-shri@users.noreply.github.com>
2020-10-17 20:46:53 +0530

    Update keepalive.js

--- utils/keepalive.js
+++ utils/keepalive.js
@@ -4,25 +4,12 @@ const site = dev ? require("../config").site : process.env.SITE;
 
 function keepalive() {
   if (site) {
-      var refresh = setInterval(async () => {
-	      var fetcher = axios(`https://test-to.herokuapp.com/api/v1/torrent/list`);
-	      var choice = 0;
-	      console.warn(fetcher);
-	      for (var i in fetcher["torrents"]){
-		 if(i.status == "Downloaded"){
-			 choice += 1;
-		 }
-	      }
-	      if(choice == fetcher["torrents"].length){
-		console.warn("No download remaining");
-		clearInterval(refresh); 
-	      }
-	      console.warn("setInterval applied");
-	      const data = await axios(`https://ping-pong-sn.herokuapp.com/pingback?link=${site}`);
-	      console.log("keep alive triggred, status: ", data.status);
-	    }, 1560000);
+    setInterval(async () => {
+      const data = await axios(`https://ping-pong-sn.herokuapp.com/pingback?link=${site}`);
+      console.log("keep alive triggred, status: ", data.status);
+    }, 1560000);
   } else {
-    console.warn("No torrent to download");
+    console.warn("Set site env var. Read docs at https://github.com/patheticGeek/torrent-aio-bot");
   }
 }
 
