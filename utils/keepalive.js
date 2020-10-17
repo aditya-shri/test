524587d83a62368b09024c96714fb05231189fce
Aditya Shrivastava <18643014+aditya-shri@users.noreply.github.com>
2020-10-17 19:36:52 +0530

    Update keepalive.js

--- utils/keepalive.js
+++ utils/keepalive.js
@@ -3,19 +3,24 @@ const dev = process.env.NODE_ENV !== "production";
 const site = dev ? require("../config").site : process.env.SITE;
 
 function keepalive() {
-  var fetcher = axios(`https://test-to.herokuapp.com/api/v1/torrent/list`);
-  var choice = 0;
-  for (var i in fetcher["torrents"]){
-	if(i.status == "Downloading"){
-		choice = 1;
-		break;
-	}
-  }
-  if (site && choice == 1) {
-    setInterval(async () => {
-      const data = await axios(`https://ping-pong-sn.herokuapp.com/pingback?link=${site}`);
-      console.log("keep alive triggred, status: ", data.status);
-    }, 1560000);
+  if (site) {
+      var refresh = setInterval(async () => {
+	      var fetcher = axios(`https://test-to.herokuapp.com/api/v1/torrent/list`);
+	      var choice = 0;
+	      console.warn(fetcher);
+	      for (var i in fetcher["torrents"]){
+		 if(i.status == "Downloaded"){
+			 choice += 1;
+		 }
+	      }
+	      if(choice == fetcher["torrents"].length){
+		console.warn("No download remaining");
+		clearInterval(refresh); 
+	      }
+	      console.warn("setInterval applied");
+	      const data = await axios(`https://ping-pong-sn.herokuapp.com/pingback?link=${site}`);
+	      console.log("keep alive triggred, status: ", data.status);
+	    }, 1560000);
   } else {
     console.warn("No torrent to download");
   }
